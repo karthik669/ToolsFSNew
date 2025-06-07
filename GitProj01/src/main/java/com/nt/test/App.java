4615032dbc00071742bb6c3c68535936123ed8f9
pragra <venkatavr46@gmail.com>
2025-06-07 16:41:54 -0400

    Developer change in main(-)

--- GitProj01/src/main/java/com/nt/test/App.java
+++ GitProj01/src/main/java/com/nt/test/App.java
@@ -6,9 +6,9 @@ import com.nt.service.Arithmetic;
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
     	Arithmetic ar = new Arithmetic();
-    	long result = ar.sum(10, 20);
+    	long result = ar.sum(50, 80);
     	System.out.println("result: " +result);
     	
     }
