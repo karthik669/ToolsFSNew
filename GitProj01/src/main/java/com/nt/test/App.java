81f15a2045f7128e8eb8717753c783ab884ab7bf
pragra <venkatavr46@gmail.com>
2025-06-08 20:03:26 -0400

    Developer correcting merge conflict in App.java

--- GitProj01/src/main/java/com/nt/test/App.java
+++ GitProj01/src/main/java/com/nt/test/App.java
@@ -8,12 +8,9 @@ import com.nt.service.Arithmetic;
 public class App {
     public static void main(String[] args) { 
     	Arithmetic ar = new Arithmetic();
-<<<<<<< HEAD
-    	long result = ar.sum(30, 40);
-=======
-    	long result = ar.sum(50, 80);
->>>>>>> branch 'master' of https://github.com/karthik669/ToolsFSNew.git
-    	System.out.println("result: " +result);
+    	long result1= ar.sum(30, 40);
+    	long result2 = ar.sum(50, 80);
+    	System.out.println("result: " +result+"..."+result2);
     	
     }
 }
