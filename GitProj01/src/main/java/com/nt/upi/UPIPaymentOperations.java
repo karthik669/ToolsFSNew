07b9fdbfcab695f8135cc0845de667dd606a8580
pragra <venkatavr46@gmail.com>
2025-06-09 13:11:45 -0400

    committing files which got unstaged after pull

--- GitProj01/src/main/java/com/nt/upi/UPIPaymentOperations.java
+++ GitProj01/src/main/java/com/nt/upi/UPIPaymentOperations.java
@@ -1,12 +1,14 @@
 package com.nt.upi;
 
+import java.util.Random;
+
 public class UPIPaymentOperations {
 
 	public String doPayment(String upiId, double amount ) {
 		return amount+"is transfered to"+upiId;
-		
+	}
 		public String addrewards(String txId) {
 			return txId+" has got" +new Random().nextInt(4000)+"rewards points have come";
 		}
-	}
+	
 }
